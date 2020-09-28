630371583c7f4e2a9cf6c9f5e0c4bec62a79f8e5
animus-221 <1516826317@qq.com>
2020-09-28 09:42:22 +0800

    '初始化数据'

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+  #tab-bar{
+    background-color: #f6f6f6;
+    display: flex;
+    text-align: center;
+  }
+</style>
